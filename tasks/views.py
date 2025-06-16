785cade4384310833f948e8e6b80806d747bbde5
Tayyib Imam Asif <tayyibimamasif@gmail.com>
2025-06-16 17:00:38 +0600

    Initial Project Done

---
+++ tasks/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse("Welcome to the task management system")
+
+def show_task(request):
+    return HttpResponse("This is our task page")
