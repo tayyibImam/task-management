cfdfa78ceecc5ddfc0185bec9825fbad6db378f0
Tayyib Imam Asif <tayyibimamasif@gmail.com>
2025-06-25 20:20:36 +0600

    module

--- task_management/urls.py
+++ task_management/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from tasks.views import home
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", home),
     path("tasks/", include("tasks.urls"))
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,13 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Welcome to the task management system")
-
-def show_task(request):
-    return HttpResponse("This is our task page")
-
-def show_specific_task(request, id):
-    print("id", id)
-    print("id type", type(id))
-    return HttpResponse("This is specific task page ")
+def manager_dashboard(request):
+    return render(request, "dashboard/manager-dashboard.html")
+def user_dashboard(request):
+    return render(request, "dashboard/user-dashboard.html")
